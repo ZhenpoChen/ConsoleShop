80b890f015d377115a6c38d3017a36b7adb6c57b
ZhenpoChen <673214953@qq.com>
2020-12-28 21:08:11 +0800

    登入信息比对Excel表，做出循环判断，显示商品信息，12-28

--- src/Test.java
+++ src/Test.java
@@ -17,13 +17,30 @@ public class Test {
             System.out.println("你输入的密码是：" + password);
 
             /*File file=new File("D:\\LanQiao\\idealIU2019\\IdeaProjects\\ConsoleShop\\src\\UserInfo.xlsx");*/ //转成一个可变的自由的读取路径方式
-            InputStream in = Class.forName("Test").getResourceAsStream("/UserInfo.xlsx");
-            ReadExcel readExcel = new ReadExcel();//创建对象
-            User[] users = readExcel.readExcel(in);
+            InputStream inExcel = Class.forName("Test").getResourceAsStream("/UserInfo.xlsx");
+            InputStream inProduct = Class.forName("Test").getResourceAsStream("/ProductInfo.xlsx");
+            ReadExcel readExcel = new ReadExcel(); //创建对象
+            User[] users = readExcel.readExcel(inExcel);
             for (int i = 0; i < users.length; i++) {
                 if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
                     System.out.println("登入成功");
                     bool=false;
+
+                    /*显示商品信息*/
+                    ReadProduct readProduct = new ReadProduct();
+                    Product products[] = readProduct.readProduct(inProduct);
+                    for(Product product:products){
+                        System.out.print(product.getProductId());
+                        System.out.print("\t" + product.getName());
+                        System.out.print("\t" + product.getPrice());
+                        System.out.println("\t" + product.getDescription());
+                    }
+                   /* System.out.println("请输入商品ID，并会自动将其放入购物车");
+                    String productId= sc.next();
+
+                    *//*创建一个购物车数组，并存入输入的商品ID的商品的全部信息*//*
+                    Product carts[] = new Product[3];*/
+
                     break;
                 } else {
                     System.out.println("登录失败");
